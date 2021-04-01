12780e819bd58d1c06799d0e749ac1c6495c9202
daxelka <alina.dubovskaya@ul.ie>
2021-04-01 14:44:49 +0100

    move simulation plotting to a graph library

--- graph_tools.py
+++ graph_tools.py
@@ -1,3 +1,5 @@
+import networkx as nx
+import matplotlib.pyplot as plt
 from collections import Counter
 
 # Degree Distribution
@@ -14,3 +16,19 @@ def degree_dist(G):
     # Dergee distribution normalised
     # values = [float(i) / sum(values) for i in values]
     return degrees, values, keys
+
+# Plotting information spreading on networks
+def plot_simulations(G, positions, iterations):
+    color_map = []
+    for iteration in range(G.number_of_nodes()):
+        color_map.append('green')
+
+    for iteration in iterations:
+        for index, status in iteration['status'].items():
+            if status == 1:
+                color_map[index] = 'red'
+            if status == 2:
+                color_map[index] = 'grey'
+
+        nx.draw(G, positions, node_color = color_map, with_labels = True)
+        plt.show()
